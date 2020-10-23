e5d4ab682c535b48c99df6eaf6420d01b0ff1059
mczee <maxileikan@gmail.com>
2020-10-22 22:56:03 -0300

    Agregado indicadores gráficos y entrada de sensibilidad del usuario

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { DeviceMotionAccelerometerOptions } from '@ionic-native/device-motion';
 import { DeviceMotion, DeviceMotionAccelerationData } from '@ionic-native/device-motion/ngx';
 import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
-import { NativeAudio } from '@ionic-native/native-audio/ngx';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -15,30 +16,33 @@ export class HomePage implements OnInit{
   subcripcion: any;
 
   public sensibilidad: number = 1;
-  public inclinacion_x: number = 0;
-  public inclinacion_y: number = 0;
+  /* public inclinacion_x: number = 0;
+  public inclinacion_y: number = 0; */
   public x_pos: number = 0;
   public x_neg: number = 0;
   public y_pos: number = 0;
   public y_neg: number = 0;
   public centro: number = 0;
-  id: string = 'beep';
+  
+  /* public imageSource: string = '../../assets/centro_verde.png'; */
 
-  constructor(private motion: DeviceMotion, private orientacion: ScreenOrientation, private audio: NativeAudio) {
+  public cuerpoAlerta = {
+    header: "Bienvenido",
+    message: "Para comenzar presione el botón Iniciar",
+    buttons: ["ok"]
+  };
+
+  constructor(private motion: DeviceMotion, private orientacion: ScreenOrientation, private alContrl: AlertController) {
     this.orientacion.lock(this.orientacion.ORIENTATIONS.PORTRAIT);
   }
 
-  public ngOnInit() {
-    
+
+  public async ngOnInit() {
+    const alerta = await this.alContrl.create(this.cuerpoAlerta);
+    await alerta.present();
   }
 
-  ionViewWillEnter() {
-    this.audio.preloadSimple(this.id, '../../assets/audio/beep2.mp3');
-  }
-  
-  ionViewWillLeave() {
-    this.audio.unload(this.id);
-  }
+
 
   public iniciar() {
     let opciones: DeviceMotionAccelerometerOptions = {
@@ -52,65 +56,39 @@ export class HomePage implements OnInit{
   
   }
 
-  public detener(){
+  public detener() {
     this.subcripcion.unsubscribe();
   }
 
-  public reproducirBeep() {
-    this.audio.play(this.id);
-  }
-  /* public determinarInclinacion() {
+  public determinarInclinacion() {
     if (this.datos.x > this.sensibilidad) {
-      this.inclinacion_x = 1;
+      this.x_pos = 1;
+    } else {
+      this.x_pos = 0;
     }
-    if (this.datos.x < -(this.sensibilidad)) {
-      this.inclinacion_x = -1;
+    if (this.datos.x < (-(this.sensibilidad))) {
+      this.x_neg = 1;
+    } else {
+      this.x_neg = 0;
     }
-    if ((this.datos.x < this.sensibilidad) && (this.datos.x > -(this.sensibilidad))) {
-      this.inclinacion_x = 0;        
-    } 
     
 
     if (this.datos.y > this.sensibilidad) {
-      this.inclinacion_y = 1;
+      this.y_pos = 1;
+    } else {
+      this.y_pos = 0;
     }
-    if (this.datos.y < this.sensibilidad) {
-      this.inclinacion_y = -1;
-    } 
-    if ((this.datos.y < this.sensibilidad) && (this.datos.y > -(this.sensibilidad))) {
-      this.inclinacion_y = 0;        
-    }  */
+    if (this.datos.y < (-(this.sensibilidad))) {
+      this.y_neg = 1;
+    } else {
+      this.y_neg = 0;
+    }
 
-    public determinarInclinacion() {
-      if (this.datos.x > this.sensibilidad) {
-        this.x_pos = 1;
-      } else {
-        this.x_pos = 0;
-      }
-      if (this.datos.x < (-(this.sensibilidad))) {
-        this.x_neg = 1;
-      } else {
-        this.x_neg = 0;
-      }
-      
-  
-      if (this.datos.y > this.sensibilidad) {
-        this.y_pos = 1;
-      } else {
-        this.y_pos = 0;
-      }
-      if (this.datos.y < (-(this.sensibilidad))) {
-        this.y_neg = 1;
-      } else {
-        this.y_neg = 0;
-      }
-
-      if ((this.x_neg == 0) && (this.x_pos == 0) && (this.y_neg == 0) && (this.y_pos == 0)) {
-        this.centro = 1;
-        this.reproducirBeep();
-      } else {
-        this.centro = 0;
-      }
+    if ((this.x_neg == 0) && (this.x_pos == 0) && (this.y_neg == 0) && (this.y_pos == 0)) {
+      this.centro = 1;
+    } else {
+      this.centro = 0;
+    }
 
   }
 
